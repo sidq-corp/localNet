d3dccdcbd1503f9190a7983b234b7414d08a994c
sidq-corp <dimax258223@gmail.com>
2020-04-30 12:09:03 +0300

    Second commit

--- js/reg.js
+++ js/reg.js
@@ -1,5 +1,5 @@
 function correct(){
-
+	document.location.href='../index.php';
 }
 function error(){
 	
--- php/reg.php
+++ php/reg.php
@@ -4,10 +4,43 @@
 <head>
 	<title>Регестрация</title>
 	<script src="../js/reg.js"></script>
-
+	<link rel="stylesheet" href="../css/reg.css">
 </head>
 <body>
+	<?php
+	function get_len_dir(){
+		$dir = opendir('account');
+		$count = 0;
+		while($file = readdir($dir)){
+    		if($file == '.' || $file == '..' || is_dir('path/to/dir' . $file)){
+       			 continue;
+	    }
+	    $count++;
+	    return $count;
+	}
 
+	}
+	$login = $_GET['reg_login'];
+	$name = $_GET['reg_name'];
+	$pass = $_GET['reg_pass'];
+	if(strlen($login) < 6 or strlen($pass) < 6 or strlen($name) < 2 or file_exists("account/$login.id")){
+		echo "<script>error()</script>";
+		echo "Ошибка регестраци";
+	}else{
+		$id = get_len_dir() + 1;
+		// echo "$login $name $pass $id";
+		$f = fopen("account/$login.id", 'w');
+		fwrite($f, "$id\n");
+		fwrite($f, "$login\n");
+		fwrite($f, "$name\n");
+		fwrite($f, "$pass\n");
+		fclose($f);
+		echo "Успешная регестрация";
+   	    echo "<script>correct()</script>";
+
+
+	}
+?>
 </body>
 </html>
 <?php
