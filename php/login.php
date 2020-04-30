d5fd27e81b7b6acafb4bc2c2db00dc3401fff8cc
sidq-corp <dimax258223@gmail.com>
2020-04-30 12:53:38 +0300

    8 commit

--- php/login.php
+++ php/login.php
@@ -10,17 +10,20 @@
 	<?php
 	$login = $_GET['user_login'];
 	$pass_user = hash("sha256", $_GET['user_pass']);
-	if($file_exists("account/$login.id")){
+	if(file_exists("account/$login.id")){
 		$f = fopen("account/$login.id", "r");
 		$all = fread($f,  filesize("account/$login.id"));
 		list($id, $login, $name, $pass) = explode("\n", $all);
 		// echo "$id $login $name $pass";
 		if($pass == $pass_user){
+			echo "corret";
 			echo "<script>correct_login()</script>";
 		}else{
+			echo "error";
 			echo "<script>error_pass()</script>";
 		}
 	}else{
+		echo "login";
 		echo "<script>login_not_found()</script>";
 	}
 	
