b4c2ae15f16bed296d39fae07181a532d30764ff
sidq-corp <dimax258223@gmail.com>
2020-04-30 12:51:44 +0300

    login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,9 @@
 function login_not_found(){
+
+}
+function correct_login(){
+
+}
+function error_pass(){
 	
 }
--- php/login.php
+++ php/login.php
@@ -9,12 +9,17 @@
 <body>
 	<?php
 	$login = $_GET['user_login'];
-	$pass = hash("sha256", $_GET['user_pass']);
+	$pass_user = hash("sha256", $_GET['user_pass']);
 	if($file_exists("account/$login.id")){
 		$f = fopen("account/$login.id", "r");
 		$all = fread($f,  filesize("account/$login.id"));
 		list($id, $login, $name, $pass) = explode("\n", $all);
-		echo "$id $login $name $pass";
+		// echo "$id $login $name $pass";
+		if($pass == $pass_user){
+			echo "<script>correct_login()</script>";
+		}else{
+			echo "<script>error_pass()</script>";
+		}
 	}else{
 		echo "<script>login_not_found()</script>";
 	}
