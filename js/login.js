494a256e51fa33d290ae5db3be6be63725989cad
sidq-corp <dimax258223@gmail.com>
2020-04-30 18:13:35 +0300

    commit

--- js/login.js
+++ js/login.js
@@ -1,9 +1,10 @@
 function login_not_found(){
-
+	document.location.href='../index.php';
 }
 function correct_login(sk, login, id){
 	document.location.href='../main/main.php?secret=' + sk + '&login=' + login + '&id=' + id;
 }
 function error_pass(){
+	document.location.href='../index.php';
 	
 }
