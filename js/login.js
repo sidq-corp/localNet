5215aab50c58d1c2aa36fe9d8bd20f4a10de28ba
sidq-corp <dimax258223@gmail.com>
2020-05-01 14:19:58 +0300

    d

--- js/login.js
+++ js/login.js
@@ -5,6 +5,5 @@ function correct_login(sk, login, id, name){
 	document.location.href='../main/main.php?secret=' + sk + '&login=' + login + '&id=' + id + '&name=' + name;
 }
 function error_pass(){
-	document.location.href='../index.php?error=pass';
-	
+	document.location.href='../index.php?error=pass';	
 }
