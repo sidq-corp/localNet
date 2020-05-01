86aaf99f7942ebbf5a521675e3f84114b9f3af95
depozzyx <2015hhh333@gmail.com>
2020-05-01 12:05:49 +0300

    xui

--- index.php
+++ index.php
@@ -6,32 +6,47 @@
 	<script src="js/main.js"></script>
 </head>
 <body onload="init()">
-	<div id="registr">
+	<div id = 'logo' onclick="logo_drop()"></div>
+	<div id="registr" class = 'input_form'>
 		<div class="title">
-			РЕГИСТРАЦИЯ
+			Регистрация
 		</div>
-		<div id="reg_form">
+		<div id="reg_form" class = "generic_form">
 			<form action="php/reg.php" method="get">
-				<p>Логин:</p> <input  type="text" name="reg_login">
-				<p>Имя: </p>   <input type="text" name="reg_name">
-				<p>Пароль:</p> <input type="text" name="reg_pass">
-				<input type="submit" name="submit" value="Зарегистрироваться">
+				<input type="text" placeholder="Никнейм" id = "reg_name" name="reg_name">
+				<input  type="text" placeholder="Логин" id = "reg_login" name="reg_login">
+				<input type="text" placeholder="Пароль" id = "reg_pass" name="reg_pass">
+				<button name="submit" id="check_reg" class="check_but">Зарегистрироваться</button>
 			</form>
+			<button onclick="display_error('Вы ввели логин или пароль','длинна которого меньше 6 символов')" class="check_but" id="fake_check_reg" style="display: block;">Зарегистрироваться</button>
+			<div id="help_div" style='margin-top: 10%'>
+				<p onclick='show_log_form()'>Уже зарегестрированы? Вход.</p>
+				<!-- <p>Как это работает?</p> -->
+			</div>
 		</div>
 	</div>
-	<div id = 'logo' onclick="logo_drop()"tooltip = "О нас"></div>
 	<div id="login" class = 'input_form'>
 		
 		<div class="title">
 			Вход
 		</div>
-		<div id="login_form">
+		<div id="login_form" class = "generic_form">
 			<form action="php/login.php" method="get">
 				<input  type="text" name="user_login" id="user_login"  placeholder="Логин">
 				<input type="text" name="user_pass" id="user_pass" placeholder="Пароль">
-				<button name="submit" id="check">Войти</button>
+				<button name="submit" id="check_log" class="check_but" style="display: none;">Войти</button>
 			</form>
+			<button onclick="display_error('Вы ввели логин или пароль','длинна которого меньше 6 символов')" class="check_but" id="fake_check_log" style="display: block;">Войти</button>
+			<div id="help_div">
+				<p onclick='show_reg_form()'>Нет аккаунта? Регистрация.</p>
+				<!-- <p>Как это работает?</p> -->
+			</div>
 		</div>
+		
+	</div>
+	<div id="error_div" onclick="dismiss_error()">
+		<p>Вы хуйло!</p>
+		<p>Блять</p>
 	</div>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,59 +1,148 @@
 function init(){
+	cookie_check()
 	blur_setup()
+}
+// Cookie
+function cookie_check(){
+	ck = getCookie("err")
+	document.cookie = "user=PIDORS"
+	// alert(document.cookie)
+	if (ck == 'login_not_found'){
+		alert('Нет логина')
+	}
 }
 // Blur
 var oks = new Map();
 oks.set("user_pass", 0).set("user_login", 0);
 let standingby = 0
 
+var oks_reg = new Map();
+oks_reg.set("reg_pass", 0).set("reg_login", 0).set("reg_name", 0);
+
 function blur_setup(){
 	document.getElementById("user_pass").addEventListener("focus", () => check_focus("user_pass"), true);
-	document.getElementById("user_pass").addEventListener("blur", () => check_blur("user_pass"), true);
+	document.getElementById("user_pass").addEventListener("blur", () => check_blur_log("user_pass"), true);
+	document.getElementById("user_pass").addEventListener("input", () => check_blur_log("user_pass"), true);
 
 	document.getElementById("user_login").addEventListener("focus", () => check_focus("user_login"), true);
-	document.getElementById("user_login").addEventListener("blur", () => check_blur("user_login"), true);
+	document.getElementById("user_login").addEventListener("blur", () => check_blur_log("user_login"), true);
+	document.getElementById("user_pass").addEventListener("input", () => check_blur_log("user_pass"), true);
+
+	document.getElementById("reg_pass").addEventListener("focus", () => check_focus("reg_pass"), true);
+	document.getElementById("reg_pass").addEventListener("blur", () => check_blur_reg("reg_pass"), true);
+	document.getElementById("reg_pass").addEventListener("input", () => check_blur_reg("reg_pass"), true);
+
+	document.getElementById("reg_login").addEventListener("focus", () => check_focus("reg_login"), true);
+	document.getElementById("reg_login").addEventListener("blur", () => check_blur_reg("reg_login"), true);
+	document.getElementById("reg_login").addEventListener("input", () => check_blur_reg("reg_login"), true);
+
+	document.getElementById("reg_name").addEventListener("focus", () => check_focus("reg_name"), true);
+	document.getElementById("reg_name").addEventListener("blur", () => check_blur_reg("reg_name"), true);
+	document.getElementById("reg_name").addEventListener("input", () => check_blur_reg("reg_name"), true);
 }
 
 function check_focus(elem){
-	document.getElementById(elem).style.opacity = '0.7';
+	document.getElementById('fake_check_log').style.opacity = '0.5';
+	document.getElementById('check_log').style.display = 'none';
+	document.getElementById('check_log').style.opacity = '0.5';
+	document.getElementById('fake_check_log').style.display = 'block';
+
+	document.getElementById('fake_check_reg').style.opacity = '0.5';
+	document.getElementById('check_reg').style.display = 'none';
+	document.getElementById('check_reg').style.opacity = '0.5';
+	document.getElementById('fake_check_ref').style.display = 'block';
 	standingby = 0
 }
-function check_blur(elem){
+function check_blur_log(elem){
 	str = document.getElementById(elem).value
 	if (str.length >= 6) {
 		oks.set(elem, 1);
-		check_all_oks()
+		check_all_oks_log()
 	}else{
 		oks.set(elem, 0);
-		check_all_oks()
+		check_all_oks_log()
 	}
 }	
-function check_all_oks(){
+function check_all_oks_log(){
 	for (var [key, value] of oks) {
 		if (value == 0){
-			document.getElementById('check').style.opacity = '0.7';
+			document.getElementById('fake_check_log').style.opacity = '0.5';
+			document.getElementById('check_log').style.display = 'none';
+			document.getElementById('check_log').style.opacity = '0.5';
+			document.getElementById('fake_check_log').style.display = 'block';
 			standingby = 0
 			return
 		}
 	}
 	console.log('ok')
-	document.getElementById('check').style.opacity = '1';
+	document.getElementById('fake_check_log').style.opacity = '1';
+	document.getElementById('check_log').style.display = 'block';
+	document.getElementById('check_log').style.opacity = '1';
+	document.getElementById('fake_check_log').style.display = 'none';
 	standingby = 1
 }
+function check_blur_reg(elem){
+	str = document.getElementById(elem).value
+	if ((str.length >= 6) || ((elem == 'reg_name') && (str.length >= 3))) {
+		oks_reg.set(elem, 1);
+		check_all_oks_reg()
+	}else{
+		oks_reg.set(elem, 0);
+		check_all_oks_reg()
+	}
+}	
+function check_all_oks_reg(){
+	for (var [key, value] of oks_reg) {
+		if (value == 0){
+			document.getElementById('fake_check_reg').style.opacity = '0.5';
+			document.getElementById('check_reg').style.display = 'none';
+			document.getElementById('check_reg').style.opacity = '0.5';
+			document.getElementById('fake_check_reg').style.display = 'block';
+			standingby = 0
+			return
+		}
+	}
+	console.log('ok')
+	document.getElementById('fake_check_reg').style.opacity = '1';
+	document.getElementById('check_reg').style.display = 'block';
+	document.getElementById('check_reg').style.opacity = '1';
+	document.getElementById('fake_check_reg').style.display = 'none';
+}
 
+// Display errors
+function display_error(arg1,arg2){
+	msg = ''
+	if (arg1){
+		msg = msg + '<p>'+arg1+'</p>'
+	}
+	if (arg2){
+		msg = msg + '<p>'+arg2+'</p>'
+	}
+	document.getElementById('error_div').innerHTML = msg;
+	document.getElementById('error_div').style.opacity = '1';
+	document.getElementById('error_div').style.transform = 'translateX(0px)';
+}
+function dismiss_error(){
+	document.getElementById('error_div').style.opacity = '0';
+	document.getElementById('error_div').style.transform = 'translateX(-30px)';
+}
+// Login - Register link
+function show_reg_form(){
+	dismiss_error()	
+	document.getElementById('login').style.display = 'none';
+	document.getElementById('registr').style.display = 'block';
+}
+function show_log_form(){
+	dismiss_error()	
+	document.getElementById('login').style.display = 'block';
+	document.getElementById('registr').style.display = 'none';
+}
 
-
-
-
+// Animations
 function logo_drop(){
 	console.log('los')
 	sdrop(document.getElementById('logo'));
-	setTimeout(logo_drop_f2,1000)
 }	
-function logo_drop_f2(){
-	// document.getElementById('logo').style.transtion = "transform 3s linear"
-	// document.getElementById('logo').style.transform = "translateY(500%)"
-}
 
 function shake(elem) {
 	elem.classList.add("apply-shake");
@@ -66,4 +155,12 @@ function sdrop(elem) {
 	elem.addEventListener("animationend", (e) => {
 	    elem.classList.add("apply-drop-end");
 	});
+}
+
+// SHIT
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
