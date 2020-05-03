6ee0844fdb721a496aad3dc998af88787cc5d2b0
depozzyx <2015hhh333@gmail.com>
2020-05-03 15:19:57 +0300

    dd

--- js/main.js
+++ js/main.js
@@ -124,26 +124,3 @@ function gui_account_close(){
 
 // Generic errors
 // Display errors
-function display_error(arg1,arg2){
-	msg = '<br><p>'
-	document.getElementById('error_div').style.backgroundColor = 'rgba(245, 70, 12, 0.7)';
-
-	if (arg1){
-		msg = msg + ''+arg1+''
-	}
-	if (arg2 == 1){
-		document.getElementById('error_div').style.backgroundColor = 'rgba(31, 173, 34, 0.7)';
-	}else if (arg2){
-		msg = msg + ' '+arg2+''
-	}
-	msg = msg + '</p><br>	'
-	document.getElementById('error_div').innerHTML = msg;
-	document.getElementById('error_div').style.opacity = '1';
-	document.getElementById('error_div').style.transform = 'translateX(0px)';
-
-	setTimeout(dismiss_error, 5000)
-}
-function dismiss_error(){
-	document.getElementById('error_div').style.opacity = '0';
-	document.getElementById('error_div').style.transform = 'translateX(-30px)';
-}
