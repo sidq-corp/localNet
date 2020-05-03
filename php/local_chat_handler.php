eaaafb91c3c40f1e34eb5a8bfad7991b80fbafe1
sidq-corp <dimax258223@gmail.com>
2020-05-03 19:36:26 +0300

    cc

--- php/local_chat_handler.php
+++ php/local_chat_handler.php
@@ -5,8 +5,12 @@
 	$mess = $_GET['mess'];
 	if(file_exists("messlog/localchat/$me-$to.log")){
 		if($mess != ''){
-			$f = fopen("messlog/localchat/$me-$to.log", "a");
-			fwrite($f, "$me: $mess<br> \n");
+			$f = fopen("messlog/localchat/$me-$to.log", "r");
+			$text = fread($f, filesize("messlog/localchat/$me-$to.log"));
+			fclose($f);
+			$mess = $mess . "<br>\n" . $text;
+			$f = fopen("messlog/localchat/$me-$to.log", "w");
+			fwrite($f, "$me: $mess");
 			fclose($f);
 		}
 		$f = fopen("messlog/localchat/$me-$to.log", "r");
@@ -15,8 +19,12 @@
 
 	}elseif(file_exists("messlog/localchat/$to-$me.log")){
 		if($mess != ''){
-			$f = fopen("messlog/localchat/$to-$me.log", "a");
-			fwrite($f, "$me: $mess<br> \n");
+			$f = fopen("messlog/localchat/$to-$me.log", "r");
+			$text = fread($f, filesize("messlog/localchat/$to-$me.log"));
+			fclose($f);
+			$mess = $mess . "<br>\n" . $text;
+			$f = fopen("messlog/localchat/$to-$me.log", "w");
+			fwrite($f, "$me: $mess");
 			fclose($f);
 		}
 		$f = fopen("messlog/localchat/$to-$me.log", "r");
