045fb4199c388a9030051aea88a2127381780f85
sidq-corp <dimax258223@gmail.com>
2020-05-09 17:09:56 +0300

    Ss

--- php/media.php
+++ php/media.php
@@ -4,6 +4,7 @@
 	<title>Медиа</title>
 </head>
 <body>
+	<h1>Add</h1>
 	<form enctype="multipart/form-data" action="media.php" method="POST">
 		  Ритм: <input name="rhythm" type="range" min="1" max="100" value="50" class="slider" id="myRange"><br>
 		  Мелодия: <input name="melody" type="range" min="1" max="100" value="50" class="slider" id="myRange"><br>
@@ -11,7 +12,7 @@
 		  Адекватность: <input name="adequacy" type="range" min="1" max="100" value="50" class="slider" id="myRange"><br>
 		  Звучность: <input name="ringing" type="range" min="1" max="100" value="50" class="slider" id="myRange"><br>
 		  Исполнитель: <input type="text" name="singer"><br>
-		  Название: <input type="text" name="name">
+		  Название: <input type="text" name="name"><br>
    		  Отправить этот файл: <input name="userfile" type="file"><br>
 
 		  <input type="submit" name="submit">
@@ -25,18 +26,27 @@
 			if(!file_exists($uploadfile)){
 				if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
 			    	echo "Файл корректен и был успешно загружен.\n";
-			    	add_sound($uploadfile, $_POST['name'], $_POST['singer'], $_GET['rhythm'], $_GET['melody'], $_GET['bass'], $_GET['adequacy'], $_GET['ringing']);
+			    	add_sound($uploadfile, $_POST['name'], $_POST['singer'], $_POST['rhythm'], $_POST['melody'], $_POST['bass'], $_POST['adequacy'], $_POST['ringing']);
 			    // break;
 				} else {
 					echo "Ошибка";
 				}
 			} else {
-				    echo "Файл сущвствует";
+				    echo "Файл сущевствует";
 			}
 		// }
 		// print_r($_FILES);
 
 	}
 	?>
+	<h1>Search</h1>
+	<form action="search_audio.php" method="GET">
+		  Ритм: <input name="rhythm" type="range" min="1" max="100" value="50" class="slider" id="myRange"><br>
+		  Мелодия: <input name="melody" type="range" min="1" max="100" value="50" class="slider" id="myRange"><br>
+		  Басс: <input name="bass" type="range" min="1" max="100" value="50" class="slider" id="myRange"><br>
+		  Адекватность: <input name="adequacy" type="range" min="1" max="100" value="50" class="slider" id="myRange"><br>
+		  Звучность: <input name="ringing" type="range" min="1" max="100" value="50" class="slider" id="myRange"><br>
+		  <input type="submit" name="submit">
+	</form>
 </body>
 </html>
