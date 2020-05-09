1ea7d21e2d47a0f70e2a0b98540120d03b17db8b
depozzyx <mosentsowivan@gmail.com>
2020-05-09 09:39:49 +0300

    dsdsd

--- js/games.js
+++ js/games.js
@@ -1,7 +1,7 @@
 function init_games(){
 	// height = document.
 }
-var links = ['../games/2048/index.html', '../games/AlienInvasion/index.html'];
+var links = ['../games/2048/index.html', '../games/AlienInvasion/index.html', 'media.php'];
 function link(arg){
 	window.location.href = links[arg]
 }
